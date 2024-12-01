3332952ad429832e8e833d0fc29a4438a77940d8
Timur <tima@MacBook-Pro-Timur-2.local>
2024-12-01 20:39:48 +0400

    dffdg

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,10 +1,10 @@
-$(function () {
-    const numberOfDivs = 18; // Number of <div> elements you want
-    const $container = $('#shelf'); // Target the container element
+const sizeOfShelfElemDesktop = 160;
+const sizeOfShelfElemMobile = 90;
+let shelf = $("#shelf");
 
-    for (let i = 0; i < numberOfDivs; i++) {
-        $container.prepend('<div class="cell"></div>'); // Append each <div>
-    }
+$(function () {
+
+    fillShelf(shelf.width(), shelf.height());
 });
 
 let tabs = $(".tab.pixel-corners");
@@ -25,11 +25,35 @@ tabs.on("mousedown", function () {
         return;
     }
     $(this).addClass("active");
-    $(this).parent().addClass("active");
 });
 
 tabs.on("mouseup", function () {
+    tabs.removeClass("active");
+    tabs.parent().removeClass("active");
+    $(this).addClass("active");
+    $(this).parent().addClass("active");
 });
+
+$(window).on("resize", function () {
+    fillShelf(shelf.width(), shelf.height());
+})
+
+function fillShelf(width, height) {
+    shelf.empty();
+
+    let sizeOfShelfElem = sizeOfShelfElemDesktop;
+    if($(window).width() < 768){
+        sizeOfShelfElem = sizeOfShelfElemMobile;
+    }
+    let columnCount = Math.floor((width / sizeOfShelfElem));
+    let rowCount = Math.floor((height / sizeOfShelfElem));
+    let numberOfDivs = Math.floor((columnCount * rowCount) / columnCount) * columnCount;
+    for (let i = 0; i < numberOfDivs; i++) {
+        shelf.prepend('<div class="cell"></div>');
+    }
+    shelf.css("grid-template-column", `repeat(${columnCount}, 1fr)`)
+    shelf.css("grid-template-rows", `repeat(${rowCount}, 1fr)`)
+}
 
 
 
